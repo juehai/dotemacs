96f15709b5d5dce3cd792ee106fd3ab71b7321c4
Jianing Yang <jianingy@unitedstack.com>
2014-01-29 16:58:50 +0800

    add org mode presentation support

---
+++ conf-available/org-presentation.el
@@ -0,0 +1,10 @@
+;;; org-presentation --- org-mode presentation configuration
+;;; Commentary:
+;;; Code:
+
+(nby/add-to-load-path "lisp/vendor/org-reveal")
+(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/2.5.0/")
+(require 'ox-reveal)
+
+
+;;; org-presentation ends here
