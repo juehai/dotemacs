bc6886c0463ec0512d80743b4ab7515e3019cd2b
Jianing Yang <jianingy@unitedstack.com>
2014-02-05 17:12:57 +0800

    change w3m theme

--- conf-available/browser.el
+++ conf-available/browser.el
@@ -7,6 +7,14 @@
  (custom-set-variables
   '(w3m-default-display-inline-images t)
   '(browse-url-browser-function 'w3m-browse-url)
-  '(w3m-command "w3m")))
+  '(w3m-command "w3m"))
+
+ (nby/with-feature
+  'tomorrow-night-theme
+  (color-theme-tomorrow--with-colors
+   'night
+   (custom-set-faces
+    `(w3m-anchor ((t (:foreground ,blue))))
+    `(w3m-current-anchor ((t (:foreground ,blue))))))))
 
 ;;; browser.el ends here
