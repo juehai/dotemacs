d7133e27ab61ee4f64e6241aa151b3112dcf979a
Jianing Yang <jianingy@unitedstack.com>
2014-02-06 15:32:58 +0800

    fix w3m installation; add newsticker

--- conf-available/browser.el
+++ conf-available/browser.el
@@ -2,12 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
+(nby/require 'w3m :package-name 'emacs-w3m)
 (nby/with-feature
  'w3m
  (custom-set-variables
   '(w3m-default-display-inline-images t)
   '(browse-url-browser-function 'w3m-browse-url)
   '(w3m-command "w3m"))
+
+ ;; RSS Browser with W3M. newsticker is an emacs builtin feature
+ (require 'newsticker)
+
+ (setq newsticker-html-renderer 'w3m-region)
+ (setq newsticker-retrieval-interval 600)
+ (setq newsticker-url-list-defaults nil)
+ (global-set-key (kbd "C-c r") 'newsticker-treeview)
 
  (nby/with-feature
   'tomorrow-night-theme
