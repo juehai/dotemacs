1db6b04f8a023a0cbb5665683828050fd7eafc65
Jianing Yang <jianingy@unitedstack.com>
2014-02-08 21:57:23 +0800

    Fixed YASnippet Configuration Error

--- conf-available/autocomplete.el
+++ conf-available/autocomplete.el
@@ -4,23 +4,6 @@
 ;;;   2. configuration for yasnippet
 ;;; Code:
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Auto-complete
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(nby/with-feature
- 'auto-complete
- (custom-set-variables
-  '(ac-comphist-file (nby/build-relative-path "db/ac-comphist.el")))
- (when (require 'auto-complete-config nil t)
-   (ac-config-default)
-   (global-auto-complete-mode t)
-   (define-key ac-complete-mode-map "\t" 'ac-expand)
-   (define-key ac-complete-mode-map "\r" nil)))
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -34,10 +17,31 @@
   '(yas-snippet-dirs
        `(,(nby/build-relative-path "snippets")
          ,(nby/build-relative-path "el-get/yasnippet/snippets"))))
- (yas-global-mode t)
- (define-key ac-complete-mode-map "\C-n" 'ac-next)
- (define-key ac-complete-mode-map "\C-p" 'ac-previous)
- (define-key ac-complete-mode-map "\t" 'ac-complete)
- (define-key ac-complete-mode-map "\r" nil))
+ (yas-global-mode t))
+(nby/with-feature 'auto-complete-yasnippet)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Auto-complete
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(nby/with-feature
+ 'auto-complete
+ (custom-set-variables
+  '(ac-comphist-file (nby/build-relative-path "db/ac-comphist.el")))
+ (when (require 'auto-complete-config nil t)
+   (ac-config-default)
+   (global-auto-complete-mode t)
+;   (define-key ac-complete-mode-map "\t" 'ac-expand)
+   (define-key ac-complete-mode-map "\C-n" 'ac-next)
+   (define-key ac-complete-mode-map "\C-p" 'ac-previous)
+   (define-key ac-complete-mode-map "\t" 'ac-complete)
+   (define-key ac-complete-mode-map "\r" nil)))
+
+
+
+
 
 ;;; autocomplete ends here
