613a7bc117ca510cf656f4b8e7e6ded7ebe9f8d0
Jianing Yang <jianingy.yang@gmail.com>
2014-06-08 10:49:20 +0800

    auto find venv

--- conf-available/lang-python.el
+++ conf-available/lang-python.el
@@ -19,6 +19,45 @@
   `(pylookup-db-file ,(nby/build-relative-path "/db/pylookup.db")))
  (global-set-key "\C-ch" 'pylookup-lookup))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Jedi-Helpers: Enable virtualenv based on project root directory
+;; Copied from: https://github.com/tkf/emacs-jedi/issues/123
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun nby/python-project-directory (buffer-name)
+  "Returns the root directory of the project that contains the
+given buffer. Any directory with a .git or .jedi file/directory
+is considered to be a project root."
+  (interactive)
+  (let ((root-dir (file-name-directory buffer-name)))
+    (while (and root-dir
+                (not (file-exists-p (concat root-dir ".git")))
+                (not (file-exists-p (concat root-dir ".jedi"))))
+      (setq root-dir
+            (if (equal root-dir "/")
+                nil
+              (file-name-directory (directory-file-name root-dir)))))
+    root-dir))
+
+(defun nby/python-project-name (buffer-name)
+  "Returns the name of the project that contains the given buffer."
+  (let ((root-dir (nby/python-project-directory buffer-name)))
+    (if root-dir
+        (file-name-nondirectory
+         (directory-file-name root-dir))
+      nil)))
+
+(defun nby/jedi-setup-venv ()
+  "Activates the virtualenv of the current buffer."
+  (let ((project-name (nby/python-project-name buffer-file-name)))
+    (when project-name
+      (progn
+        (message (concat
+                  "use virtualenv: "
+                  project-name))
+        (venv-workon project-name)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -81,6 +120,7 @@
  'python-mode
  (nby/with-feature
   'jedi
+  (add-hook 'python-mode-hook 'nby/jedi-setup-venv)
   (add-hook 'python-mode-hook 'jedi:setup)
   (setq jedi:setup-keys nil
 	jedi:complete-on-dot t)))
