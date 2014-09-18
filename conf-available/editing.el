99ea94d1005fcfb6611683e32cd638aeeccc0213
Jianing Yang <jianing.yang@qunar.com>
2014-09-18 12:18:15 +0800

    add multiple cursor mode

--- conf-available/editing.el
+++ conf-available/editing.el
@@ -139,4 +139,18 @@ user."
       (insert "--8<-----------------------separator------------------>8---")
       (comment-region cur (point)))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Multiple Cursors Mode: Edit multiple line at once
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(nby/with-feature
+ 'multiple-cursors
+ (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+ (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+ (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+ (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
 ;;; editing.el ends here
