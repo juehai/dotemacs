79a46c0cdb6e319c1e3b6cc581946c8bff418547
Jianing Yang <jianing.yang@qunar.com>
2015-02-09 11:34:59 +0800

    update w3m conf

--- conf-available/browser.el
+++ conf-available/browser.el
@@ -5,10 +5,9 @@
 (nby/require 'w3m :package-name 'emacs-w3m)
 (nby/with-feature
  'w3m
- (custom-set-variables
-  '(w3m-default-display-inline-images t)
-  '(browse-url-browser-function 'w3m-browse-url)
-  '(w3m-command "w3m"))
+ (setq w3m-default-display-inline-images t
+       ;; browse-url-browser-function 'w3m-browse-url
+       w3m-command "w3m")
 
  ;; RSS Browser with W3M. newsticker is an emacs builtin feature
  (require 'newsticker)
@@ -19,8 +18,6 @@
  (global-set-key (kbd "C-c r") 'newsticker-treeview)
 
  (nby/with-current-theme-colors
-  (custom-set-faces
-   `(w3m-anchor ((t (:foreground ,blue))))
-   `(w3m-current-anchor ((t (:foreground ,blue)))))))
-
+  (set-face-attribute 'w3m-anchor nil :foreground blue)
+  (set-face-attribute 'w3m-current-anchor nil :foreground blue)))
 ;;; browser.el ends here
