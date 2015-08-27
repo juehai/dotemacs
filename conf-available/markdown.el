bb0808fc824c12c5bde42df9f3ee15d0bfdb19be
Jianing Yang <jianingy.yang@gmail.com>
2015-08-27 17:29:30 +0800

    add markdown support

---
+++ conf-available/markdown.el
@@ -0,0 +1,8 @@
+(nby/add-to-load-path "lisp/vendor/livedown")
+(nby/with-feature
+ 'livedown
+(custom-set-variables
+ '(livedown:autostart nil) ; automatically open preview when opening markdown files
+ '(livedown:open t)        ; automatically open the browser window
+ '(livedown:port 1337))    ; port for livedown server
+)
