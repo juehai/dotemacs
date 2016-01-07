8dbc15732b5a3502bf982bd1674762403136da26
Jianing Yang <jianingy.yang@gmail.com>
2016-01-07 11:18:05 +0800

    update python template to a funny one

--- templates/template.py
+++ templates/template.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
+# This piece of code is written by
+#    `user-full-name` <`user-mail-address`>
+# with love and passion!
 #
-# Copyright `(format-time-string "%Y")`, `license-organization`
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-#
-# Author: `user-full-name` <`user-mail-address`>
-#
+#        H A P P Y    H A C K I N G !
+#              _____               ______
+#     ____====  ]OO|_n_n__][.      |    |
+#    [________]_|__|________)<     |YANG|
+#     oo    oo  'oo OOOO-| oo\\\\_   ~~~|~~~
+# +--+--+--+--+--+--+--+--+-$1-+--+--+--+--+
+#                                     `(format-time-string "%Y")`
