435e1453cfade9d3c733e2f7ae752a9e3cd2dd8f
Jianing Yang <jianingy.yang@gmail.com>
2016-01-08 10:25:28 +0800

    update python default template

--- templates/template.py
+++ templates/template.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#
 # This piece of code is written by
 #    `user-full-name` <`user-mail-address`>
 # with love and passion!
@@ -7,6 +8,7 @@
 #              _____               ______
 #     ____====  ]OO|_n_n__][.      |    |
 #    [________]_|__|________)<     |YANG|
-#     oo    oo  'oo OOOO-| oo\\\\_   ~~~|~~~
-# +--+--+--+--+--+--+--+--+-$1-+--+--+--+--+
-#                                     `(format-time-string "%Y")`
+#     oo    oo  'oo OOOO-| oo\\\\_   ~o~~~o~
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+
+#                             `(format-time-string "%e %b, %Y")`
+#
