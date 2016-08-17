f3c4cf08687436c431239f7fe88ae3215d023cdc
Jianing Yang <jianingy.yang@gmail.com>
2016-08-17 19:56:29 +0800

    add run-and-compile-rust

--- conf-available/coding.el
+++ conf-available/coding.el
@@ -258,6 +258,17 @@
  'rust-mode
  ; (add-hook 'rust-mode-hook 'cargo-minor-mode)
 
+ (defun rust-save-compile-and-run ()
+   "Compile buffer content and run."
+   (interactive)
+   (save-buffer)
+   (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
+       (compile "cargo run")
+     (compile (format "rustc %s && %s"
+                      (buffer-file-name)
+                      (file-name-sans-extension (buffer-file-name))))))
+ (define-key rust-mode-map (kbd "C-c C-c") 'rust-save-compile-and-run)
+
  (nby/with-feature
   'flycheck-rust
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
