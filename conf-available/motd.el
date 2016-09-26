09517a6d358e3266d352854c0d46dee7837d87a4
Jianing Yang <jianingy.yang@gmail.com>
2016-09-26 09:12:26 +0800

    tuning motd style

--- conf-available/motd.el
+++ conf-available/motd.el
@@ -3,36 +3,36 @@
 ;;; Code:
 
 
-(defun nby/get-motd ()
-  "Get all motd text and do some decoration."
-  (let* ((motds (directory-files (nby/build-relative-path "/motd") t ".txt"))
-         (selected (random (length motds)))
-         (motd (nth selected motds)))
-    (with-temp-buffer
-      (insert-file-contents motd)
-      (if (< (count-lines (point-min) (point-max)) 2)
-            (save-excursion
-              (goto-char (point-max))
-              (insert "\n\n")))
-      (lisp-interaction-mode)
-      (kill-line)
-      (next-line)
-      (save-excursion
-        (nby/with-feature
-         'boxquote
-         (boxquote-region (point) (point-max))))
-      (move-end-of-line nil)
-      (insert " Tip of the day: ")
-      (yank)
-      (comment-region (point-min) (point-max))
-      (concat (buffer-string) "\n"))))
+(nby/with-feature
+ 'boxquote
+ (defun nby/get-motd ()
+   "Get all motd text and do some decoration."
+   (let* ((motds (directory-files (nby/build-relative-path "/motd") t ".txt"))
+          (selected (random (length motds)))
+          (motd (nth selected motds)))
+     (with-temp-buffer
+       (insert-file-contents motd)
+       (save-excursion
+         (goto-char (point-max))
+         (insert "\n"))
+       (lisp-interaction-mode)
+       (insert "\n")
+       (kill-line)
+       ; (forward-line)
+       (save-excursion
+         (boxquote-region (point-min) (point-max)))
+       (move-end-of-line nil)
+       (yank)
+       (comment-region (point-min) (point-max))
+       (concat (buffer-string) "\n"))))
 
 
-(defun nby/show-motd ()
-  "Display a random motd message."
-  (interactive)
-  (message (nby/get-motd)))
+ (defun nby/show-motd ()
+   "Display a random motd message."
+   (interactive)
+   (message (nby/get-motd)))
 
-(setq initial-scratch-message (nby/get-motd))
+ (setq initial-scratch-message (nby/get-motd))
+)
 
 ;;; motd.el ends here
